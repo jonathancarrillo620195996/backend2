1acef2b48bfc924480869d234392bfc004cb51e0
jonathancarrillo620195996 <jonathanpinguino@gmail.com>
2025-12-02 09:56:47 -0600

    Implementación de MongoDB, dotenv y middlewares en Backend2 intento 3

--- models/persons.js
+++ models/persons.js
@@ -10,8 +10,8 @@ mongoose.connect(url)
   .then(() => {
     console.log('Connected to MongoDB')
   })
-  .catch((err) => {
-    console.log('Error connecting to MongoDB:', err.message)
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
